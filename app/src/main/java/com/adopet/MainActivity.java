bff4eab73e85aebab5b923a8603f3c50c2743471
VitNecha <vtnchk1@gmail.com>
2020-08-10 23:47:40 +0300

    Bottom navigation bar update (early version).

--- app/src/main/java/com/adopet/MainActivity.java
+++ app/src/main/java/com/adopet/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.adopet;
+package com.adopet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.adopet.R;
 
 public class MainActivity extends AppCompatActivity {
 
