5fd881464f81a42f9c5bbefb71a126e41a10fa71
VitNecha <41152474+VitNecha@users.noreply.github.com>
2020-08-12 15:01:56 +0300

    Colors updated Fragments Added Temporary pictures added

--- app/src/main/java/com/adopet/MainActivity.java
+++ app/src/main/java/com/adopet/MainActivity.java
@@ -1,16 +1,47 @@
 package com.adopet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.adopet.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedTab = null;
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    selectedTab = new HomeFragment();
+                    break;
+                case R.id.navigation_search:
+                    selectedTab = new SearchFragment();
+                    break;
+                case R.id.navigation_info:
+                    selectedTab = new InfoFragment();
+                    break;
+                case R.id.navigation_settings:
+                    selectedTab = new SearchFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_container, selectedTab).commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+        bottomNavigation.setSelectedItemId(R.id.navigation_home);
     }
 }
