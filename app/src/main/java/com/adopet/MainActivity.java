9b9c455c18b62e84b62a9450a72dc4dfc3708cd4
VitNecha <41152474+VitNecha@users.noreply.github.com>
2020-09-02 17:31:17 +0300

    Toasty + Tasty Toast notification design added. Firebase connected (Checked). Start of redirection (rtl + hebrew).

--- app/src/main/java/com/adopet/MainActivity.java
+++ app/src/main/java/com/adopet/MainActivity.java
@@ -11,10 +11,18 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 
 import com.example.adopet.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.sdsmdg.tastytoast.TastyToast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth userAuth;
+    private FirebaseUser currentUser;
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -43,8 +51,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Anonymous authentication
+        userAuth = FirebaseAuth.getInstance();
+        userAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    currentUser = userAuth.getCurrentUser();
+                    String uid = currentUser.getUid();
+                    TastyToast.makeText(getApplicationContext(), "Welcome " + uid, TastyToast.LENGTH_SHORT,TastyToast.SUCCESS);
+                }
+                else {
+                    TastyToast.makeText(getApplicationContext(),"Ooops", TastyToast.LENGTH_SHORT,TastyToast.ERROR);
+                }
+            }
+        });
+
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
         bottomNavigation.setSelectedItemId(R.id.navigation_home);
     }
+
 }
--- app/src/main/java/com/adopet/SettingsFragment.java
+++ app/src/main/java/com/adopet/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.adopet;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,18 @@ import androidx.fragment.app.Fragment;
 
 import com.example.adopet.R;
 
+import java.util.Locale;
+
 public class SettingsFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getChildFragmentManager().beginTransaction().add(R.id.settings_container, new SettingsList()).commit();
+        Configuration configuration = getResources().getConfiguration();
+        configuration.setLayoutDirection(new Locale("rtl"));
+        configuration.setLocale(new Locale("heb"));
+        getResources().updateConfiguration(configuration, getResources().getDisplayMetrics());
         return inflater.inflate(R.layout.fragment_settings, container,false);
     }
 }
